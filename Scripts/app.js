3972dc4b51769dc1d250506649ce7a2ce8b186d1
Cody Chaplin <cody.chaplin@dcmail.ca>
2022-01-21 17:32:52 -0500

    updated ICE2

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,6 +11,33 @@
         });
     }
 
+    // get reference/insertion point
+    let MainContent = document.getElementsByTagName("main")[0];
+    let DocumentBody = document.body;
+
+    // create HTML elements
+    let MainParagraph = document.createElement("p");
+    let Article = document.createElement("article");
+    let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the article paragraph</p>`;
+
+    // configure elements
+    MainParagraph.setAttribute("id", "MainParagraph");
+    MainParagraph.setAttribute("class", "mt-3");
+    let AString = "This is";
+    let AnotherString = `${AString} the main paragraph!`
+    MainParagraph.textContent = AnotherString;
+    Article.setAttribute("class", "container");
+
+
+    
+    // insert element
+    MainContent.appendChild(MainParagraph);
+    Article.innerHTML = ArticleParagraph;
+    DocumentBody.appendChild(Article);
+
+    // delete element
+    //document.getElementById("AboutUsButton").remove();
+
     // named function
     function Start()
     {
